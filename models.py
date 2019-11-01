febe886a40ea7c710da4892bd0e7dcd6c2d179d6
Emmanuelaaron <emmanuelisabirye9@gmail.com>
2019-11-01 15:42:03 +0300

    fyyur project work

---
+++ models.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template, request, Response, flash, redirect, url_for
+from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
+import logging
+from logging import Formatter, FileHandler
+from flask_wtf import Form
+from forms import *
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+moment = Moment(app)
+app.config.from_object('config')
+db = SQLAlchemy(app)
+
+# TODO: connect to a local postgresql database
+migrate = Migrate(app, db)
+
+
+
+class Show(db.Model):
+  __tablename__ = 'shows'
+  id = db.Column(db.Integer, primary_key=True)
+  venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'))
+  Artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'))
+  #artist = db.relations
+  start_time = db.Column(db.DateTime, nullable=False)
+
+
+class Venue(db.Model):
+    __tablename__ = 'Venue'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    address = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    # image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+    shows = db.relationship('Show', backref='showss', lazy=True)
+
+    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+
+class Artist(db.Model):
+    __tablename__ = 'Artist'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    # genres = db.Column(db.String(120))
+    # image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+    shows = db.relationship('Show', backref='shows', lazy=True)
